9ba00835bdfde52b5f39a11eb29a6398e313c5c1
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2017-10-01 23:07:21 +0200

    Added command line parsing

--- src/FFStats.Main/Program.cs
+++ src/FFStats.Main/Program.cs
@@ -1,13 +1,77 @@
-﻿using FFStats.DbHandler;
+﻿using FFStats.Add;
+using FFStats.DbHandler;
+using Microsoft.Extensions.CommandLineUtils;
 using System;
+using System.Collections.Generic;
 
 namespace FFStats.Main
 {
+    struct Settings
+    {
+        public List<string> TeamsToAdd { get; set; }
+        public string ScheduleFile { get; set; }
+        public List<string> LineupFiles { get; set; }
+
+        public bool IsShowingInformation { get; set; }
+    }
+
     class Program
     {
+        static Settings ParseCommandLine(string[] args)
+        {
+            var cmdLineApp = new CommandLineApplication
+            {
+                Name = "FFStats"
+            };
+            CommandArgument addTeamsArg = null;
+            CommandArgument addScheduleArg = null;
+            CommandArgument addLineupsArg = null;
+            cmdLineApp.Command("add", (addConfig) =>
+            {
+                addConfig.Description = "Add schedule or lineups";
+                addConfig.Command("schedule", (addScheduleConfig) =>
+                {
+                    addScheduleConfig.Description = "Add schedule";
+                    addScheduleArg = addScheduleConfig.Argument("file", "Schedule json file");
+                    addScheduleConfig.HelpOption("-h | --help");
+                });
+                addConfig.Command("lineups", (addLineupsConfig) =>
+                {
+                    addLineupsConfig.Description = "Add lineups";
+                    addLineupsArg = addLineupsConfig.Argument("files", "Lineup json files", multipleValues: true);
+                    addLineupsConfig.HelpOption("-h | --help");
+                });
+                addConfig.Command("teams", (addTeamsConfig) =>
+                {
+                    addTeamsConfig.Description = "Add teams";
+                    addTeamsArg = addTeamsConfig.Argument("teams", "Teams to add", multipleValues: true);
+                    addTeamsConfig.HelpOption("-h | --help");
+                });
+                addConfig.HelpOption("-h | --help");
+            });
+            cmdLineApp.HelpOption("-h | --help");
+            cmdLineApp.Execute(args);
+
+            return new Settings
+            {
+                TeamsToAdd = addTeamsArg.Values,
+                ScheduleFile = addScheduleArg.Value,
+                LineupFiles = addLineupsArg.Values,
+                IsShowingInformation = cmdLineApp.IsShowingInformation
+            };
+        }
+
         static void Main(string[] args)
         {
-            var dbHandler = new FFStatsDbHandler();
+            var settings = ParseCommandLine(args);
+
+            if (settings.IsShowingInformation)
+            {
+                Console.ReadLine();
+                return;
+            }
+
+            Add.Methods.AddTeams(settings.TeamsToAdd);
             
             Console.WriteLine("DONE");
             Console.ReadLine();
