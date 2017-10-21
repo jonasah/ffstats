8c31ef90827eef96b1698f5352d21c1c00123da3
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2017-10-21 17:11:00 +0200

    Set values on enum fields

--- src/FFStats.Models/Position.cs
+++ src/FFStats.Models/Position.cs
@@ -6,14 +6,14 @@ namespace FFStats.Models
 {
     public enum Position
     {
-        QB,
-        RB,
-        WR,
-        TE,
-        K,
-        DEF,
-        FLX,
-        BN,
-        RES
+        QB = 0,
+        RB = 1,
+        WR = 2,
+        TE = 3,
+        K = 4,
+        DEF = 5,
+        FLX = 6,
+        BN = 7,
+        RES = 8
     }
 }
