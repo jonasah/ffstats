527f8db85185e75d4ad41f3c9baddd579b9d2efc
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2017-10-21 17:15:04 +0200

    Added lineup and player handlers

---
+++ src/FFStats.DbHandler/LineupHandler.cs
@@ -0,0 +1,31 @@
+﻿using FFStats.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FFStats.DbHandler
+{
+    public static class LineupHandler
+    {
+        public static LineupPlayer AddPlayer(LineupPlayer player)
+        {
+            using (var db = new FFStatsDbContext())
+            {
+                db.LineupPlayers.Add(player);
+                db.SaveChanges();
+            }
+
+            return player;
+        }
+
+        public static void DeleteLineupsInWeek(int year, int week)
+        {
+            using (var db = new FFStatsDbContext())
+            {
+                db.RemoveRange(db.LineupPlayers.Where(lp => lp.Year == year && lp.Week == week));
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ src/FFStats.DbHandler/PlayerHandler.cs
@@ -0,0 +1,30 @@
+﻿using FFStats.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FFStats.DbHandler
+{
+    public static class PlayerHandler
+    {
+        public static Player Add(Player player)
+        {
+            using (var db = new FFStatsDbContext())
+            {
+                db.Players.Add(player);
+                db.SaveChanges();
+            }
+
+            return player;
+        }
+
+        public static Player GetByName(string name)
+        {
+            using (var db = new FFStatsDbContext())
+            {
+                return db.Players.Where(p => p.Name == name).SingleOrDefault();
+            }
+        }
+    }
+}
