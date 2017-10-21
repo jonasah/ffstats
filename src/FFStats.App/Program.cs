b0bcfc9d9c20ee1fe0845cfbef6b734ad7fd472c
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2017-10-21 22:29:07 +0200

    Moved Settings to separate file. Refactored command line parsing and added error checking

--- src/FFStats.App/Program.cs
+++ src/FFStats.App/Program.cs
@@ -1,69 +1,28 @@
-﻿using Microsoft.Extensions.CommandLineUtils;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace FFStats.App
 {
-    struct Settings
-    {
-        public string ScheduleFile { get; set; }
-        public List<string> LineupFiles { get; set; }
-
-        public bool IsShowingInformation { get; set; }
-    }
-
     class Program
     {
-        static Settings ParseCommandLine(string[] args)
-        {
-            var helpFlags = "-h | --help";
-
-            var cmdLineApp = new CommandLineApplication
-            {
-                Name = "FFStats"
-            };
-            CommandArgument addScheduleArg = null;
-            CommandArgument addLineupsArg = null;
-            cmdLineApp.Command("add", (addConfig) =>
-            {
-                addConfig.Description = "Add schedule or lineups";
-                addConfig.Command("schedule", (addScheduleConfig) =>
-                {
-                    addScheduleConfig.Description = "Add schedule";
-                    addScheduleArg = addScheduleConfig.Argument("file", "Schedule json file");
-                    addScheduleConfig.HelpOption(helpFlags);
-                });
-                addConfig.Command("lineups", (addLineupsConfig) =>
-                {
-                    addLineupsConfig.Description = "Add lineups";
-                    addLineupsArg = addLineupsConfig.Argument("files", "Lineup json files", multipleValues: true);
-                    addLineupsConfig.HelpOption(helpFlags);
-                });
-                addConfig.HelpOption(helpFlags);
-            });
-            cmdLineApp.HelpOption(helpFlags);
-            cmdLineApp.Execute(args);
-
-            return new Settings
-            {
-                ScheduleFile = addScheduleArg.Value,
-                LineupFiles = addLineupsArg.Values,
-                IsShowingInformation = cmdLineApp.IsShowingInformation
-            };
-        }
-
         static void Main(string[] args)
         {
-            var settings = ParseCommandLine(args);
+            var settings = new Settings();
 
-            if (settings.IsShowingInformation)
+            if (!settings.Parse(args))
             {
                 Console.ReadLine();
                 return;
             }
 
-            Schedule.Add(settings.ScheduleFile);
-            Lineups.AddFromFiles(settings.LineupFiles);
+            if (settings.ScheduleFile != null)
+            {
+                Schedule.Add(settings.ScheduleFile);
+            }
+            else if (settings.LineupFiles != null)
+            {
+                Lineups.AddFromFiles(settings.LineupFiles);
+            }
 
             Console.WriteLine("DONE");
             Console.ReadLine();
---
+++ src/FFStats.App/Settings.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFStats.App
+{
+    class Settings
+    {
+        public string ScheduleFile { get; set; }
+        public List<string> LineupFiles { get; set; }
+
+        public bool Parse(string[] args)
+        {
+            var helpFlags = "-h | --help";
+
+            var app = new CommandLineApplication
+            {
+                Name = "FFStats"
+            };
+            app.HelpOption(helpFlags);
+
+            // "add" command
+            app.Command("add", (addCommand) =>
+            {
+                addCommand.Description = "Add schedule or lineups";
+                addCommand.HelpOption(helpFlags);
+
+                // "add schedule" command
+                addCommand.Command("schedule", (addScheduleCommand) =>
+                {
+                    addScheduleCommand.Description = "Add schedule";
+                    addScheduleCommand.HelpOption(helpFlags);
+
+                    var scheduleFileArgument = addScheduleCommand.Argument("file", "Schedule json file");
+
+                    addScheduleCommand.OnExecute(() =>
+                    {
+                        ScheduleFile = scheduleFileArgument.Value;
+
+                        if (string.IsNullOrEmpty(ScheduleFile))
+                        {
+                            addScheduleCommand.SetError("No schedule file specified");
+                            return 1;
+                        }
+
+                        return 0;
+                    });
+                });
+
+                // "add lineups" command
+                addCommand.Command("lineups", (addLineupsCommand) =>
+                {
+                    addLineupsCommand.Description = "Add lineups";
+                    addLineupsCommand.HelpOption(helpFlags);
+
+                    var lineupFilesArgument = addLineupsCommand.Argument("files", "Lineup json files", multipleValues: true);
+
+                    addLineupsCommand.OnExecute(() =>
+                    {
+                        LineupFiles = lineupFilesArgument.Values;
+
+                        if (LineupFiles.Count == 0)
+                        {
+                            addLineupsCommand.SetError("No lineup files specified");
+                            return 1;
+                        }
+
+                        return 0;
+                    });
+                });
+
+                addCommand.OnExecute(() => {
+                    addCommand.SetError("No add command specified");
+                    return 1;
+                });
+            });
+
+            try
+            {
+                app.Execute(args);
+
+                return !app.IsShowingInformation;
+            }
+            catch (CommandParsingException)
+            {
+                return false;
+            }
+        }
+    }
+
+    static class CommandLineApplicationExtensions
+    {
+        internal static void SetError(this CommandLineApplication app, string errorMessage)
+        {
+            app.Error.WriteLine(errorMessage);
+            app.ShowHelp(); // this will set IsShowingInformation in CommandLineApplication
+        }
+    }
+}
