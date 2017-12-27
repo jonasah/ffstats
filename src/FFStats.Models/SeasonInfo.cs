2d9785b85ae5ec04a013af031a72d9e81667107b
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2017-12-27 18:36:27 +0100

    Added foreign key properties

--- src/FFStats.Models/SeasonInfo.cs
+++ src/FFStats.Models/SeasonInfo.cs
@@ -13,13 +13,24 @@ namespace FFStats.Models
         [Range(2014, int.MaxValue)]
         public int Year { get; set; }
 
+        public int? ChampionId { get; set; }
         public Team Champion { get; set; }
+
+        public int? SecondPlaceId { get; set; }
         public Team SecondPlace { get; set; }
+
+        public int? ThirdPlaceId { get; set; }
         public Team ThirdPlace { get; set; }
+
+        public int? SackoId { get; set; }
         public Team Sacko { get; set; }
 
+        public int? RegularSeasonChampionId { get; set; }
         public Team RegularSeasonChampion { get; set; }
+
+        public int? HighestPointsForTeamId { get; set; }
         public Team HighestPointsForTeam { get; set; }
+
         public double HighestPointsFor { get; set; }
     }
 }
